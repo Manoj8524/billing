63855bbb9ef485e4338a37cb02afe58923a910be
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-07-18 15:55:39 +0900

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
       dispatch({
         type: "SHOW_LOADING",
       });
-      const REACT_APP_SERVER_URL= "http://localhost:8080";
+      const REACT_APP_SERVER_URL= "https://billing-server-6caq.onrender.com";
       const res = await axios.post(
         `${REACT_APP_SERVER_URL}/api/users/login`,
         value
