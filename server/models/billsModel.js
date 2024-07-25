774bb24cfb2531534e163a7108aaa697cb48a02c
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-07-25 20:59:51 +0900

    Add files via upload

---
+++ server/models/billsModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const getCurrentTime = () => {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+ 
+  return `${hours}:${minutes}`;
+};
+
+const billSchema = mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerNumber: {
+      type: Number,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    tax: {
+      type: Number,
+      required: true,
+    },
+    paymentMode: {
+      type: String,
+      required: true,
+    },
+    cartItems: {
+      type: Array,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+    time: { type: String, default: getCurrentTime },
+  
+
+  },
+  { timestamp: true },
+);
+
+const Bills = mongoose.model("bills", billSchema);
+
+module.exports = Bills;
