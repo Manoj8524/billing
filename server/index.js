3ae6cb2a88db77a42b326bf87d855acaec182cac
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-07-26 15:19:02 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors(
   { origin:[""],
-    methods:["GET","POST","PUT","DELETE"],
+    methods:["GET","POST","PUT","DELETE","PATCH","HEAD"],
     credentials:true }
 ));
 
