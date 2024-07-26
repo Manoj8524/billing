fb22e37d7b4bb479a1191d24b694d2881d0954a9
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-07-26 18:31:26 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const statsRoutes = require("./routes/statsRoutes");
 require("colors");
 const connectDb = require("./config/config");
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000",
  methods:'GET,HEAD,PUT,PATCH,POST,DELETE'
 
 }
