99ea84c51c9c48a63c45bb42e99583be461741a1
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-07-26 21:33:33 +0900

    Delete client directory

--- client/src/Root.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Root from './Root'; // Adjust the path to your Root component
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<Root />);
