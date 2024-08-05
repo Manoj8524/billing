c40299e4465d01cffb616393a92f54389428761f
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-08-05 15:55:43 +0900

    Update CutomerPage.js

--- client/src/pages/CutomerPage.js
+++ client/src/pages/CutomerPage.js
@@ -13,7 +13,7 @@ const CustomerPage = () => {
   const getAllBills = async () => {
     try {
       dispatch({ type: "SHOW_LOADING" });
-      const REACT_APP_SERVER_URL = "https://billingserver-manoj8524s-projects.vercel.app";
+      const REACT_APP_SERVER_URL = "https://billingserver.vercel.app";
       const { data } = await axios.get(`${REACT_APP_SERVER_URL}/api/bills/get-bills`);
       setBillsData(data);
       dispatch({ type: "HIDE_LOADING" });
