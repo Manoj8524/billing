6ad27d0e13507a13c8864a538d19f556d06bfdb9
Manoj8524 <164409823+Manoj8524@users.noreply.github.com>
2024-08-05 16:32:12 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const statsRoutes = require("./routes/statsRoutes");
 require("colors");
 const connectDb = require("./config/config");
 const corsOptions = {
-  origin: "*",
+  origin: "https://billingsoft.vercel.app/login",
  methods:'GET,HEAD,PUT,PATCH,POST,DELETE'
 
 }
